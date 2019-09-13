a270edd4119cf60235fb167564f3ba7f6159233e
Binup karki <karki011@gmail.com>
2019-09-12 22:56:44 -0500

    created grid using javascript

--- connectDot.js
+++ connectDot.js
@@ -1,13 +1,71 @@
-const grid_container = document.getElementById("grid-container");
+"use strict";
+//game board  col and row
+// let gameBoard = [];
+const game_board = document.getElementById("game_board");
 
-const createGrid = function(columns, rows){
-    for(i = 1; i <=(columns * rows); i++){
+let gameboard = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]
+
+const drawBoard = function () {
+    for (let i = 1; i <= gameboard.length; i++) {
         let gridDiv = document.createElement('div');
         gridDiv.className = "grid";
-        // let node= document.createTextNode(columns, rows);
-        // gridDiv.appendChild(node);
-        grid_container.appendChild(gridDiv);
+        gridDiv.id = "grid"
+        game_board.appendChild(gridDiv);
+
+        for (let j = 1; j <= gameboard.length; j++) {
+            let gridDiv = document.createElement('div');
+            gridDiv.className = "grid";
+            gridDiv.id = "grid"
+            game_board.appendChild(gridDiv);
+        }
     }
 }
-createGrid(7, 6);
+drawBoard();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const grid_container = document.getElementById("grid-container");
+
+// const createGrid = function (columns, rows) {
+//     for (i = 1; i <= (columns * rows); i++) {
+//         let gridDiv = document.createElement('div');
+//         gridDiv.className = "grid";
+//         gridDiv.id = "grid"
+//         grid_container.appendChild(gridDiv);
+//     }
+// }
+// createGrid(7, 6);
