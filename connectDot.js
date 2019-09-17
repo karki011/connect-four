71d2ae57a69693ff60c5c87e063af31fd29b7b73
Binup karki <karki011@gmail.com>
2019-09-17 15:51:10 -0500

    added basic skeleton for game

--- connectDot.js
+++ connectDot.js
@@ -10,3 +10,67 @@
 //TODO Check whether the last disc added completed a four-in-a-row within the column (vertically).
 //TODO Check whether the last disc added completed four-in-a-row horizontally.
 //TODO Check whether the last disc added completed four-in-a-row on either an upward- or downward-sloping diagonal.
+const game_board = document.getElementById("game_board");
+let disc_color = [];
+disc_color[1] = "red";
+disc_color[2] = "black";
+let active_player = 0;
+let gameBoard = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+];
+
+function addDiscToBoard(currentPlayer, board, selectedColoumn) {
+    return
+}
+
+function displayBoard(board) {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 1; j < board.length; j++) {
+            let gridDiv = document.createElement('div');
+            gridDiv.className = "grid";
+            gridDiv.id = i+"_"+ (j-i);
+            gridDiv.setAttribute("data-col",j);
+            game_board.appendChild(gridDiv);
+        }
+    }
+}
+
+function checkGameCondition(board) {
+    //ending condition: win{who} ,tie, ""
+    return
+}
+function endGameshowMessage(who){
+
+}
+function togglePlayer(active_player) {
+    return newActive_player
+}
+
+function coloumnClickHandler(event) {
+    //figure out what colounm was clicked
+    let clickedColumn;
+    gameBoard = addDiscToBoard(active_player,gameBoard,clickedColumn);
+    displayBoard(gameBoard);
+    let condition = checkGameCondition(gameBoard);
+    if(condition!== ""){
+       endGameshowMessage(who); 
+    }else{
+        active_player = togglePlayer(active_player);
+    }
+}
+game_board.addEventListener('click', function(e){
+    console.log(e.target.id);
+    
+})
+function startGame() {
+    active_player = 1;
+    displayBoard(gameBoard);
+
+}
+startGame();
