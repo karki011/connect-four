718216287218c3564a31cc8b345dd2610102da78
Binup karki <karki011@gmail.com>
2019-09-19 15:18:37 -0500

    cleaned dir

--- final/turn.js
+++
@@ -1,5 +0,0 @@
-function setUpTurn() {
-    if (game_active) { //only run this is the game is active.
-        document.getElementById('game_info').innerHTML = "Current Player: Player " + active_player +  " <span class='player"+active_player+"'></span>";
-    }
-}	
