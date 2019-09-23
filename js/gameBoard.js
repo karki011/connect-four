41e07806c4dc22f439b332783f937725254d11dd
Binup karki <karki011@gmail.com>
2019-09-23 09:04:42 -0500

    updated css

---
+++ js/gameBoard.js
@@ -0,0 +1,24 @@
+// | 0,0 | 0,1 | 0,2 | 0,3 | 0,4 | 0,5 | 0,6 |
+// | 1,0 | 1,1 | 1,2 | 1,3 | 1,4 | 1,5 | 1,6 |
+// | 2,0 | 2,1 | 2,2 | 2,3 | 2,4 | 2,5 | 2,6 |
+// | 3,0 | 3,1 | 3,2 | 3,3 | 3,4 | 3,5 | 3,6 |
+// | 4,0 | 4,1 | 4,2 | 4,3 | 4,4 | 4,5 | 4,6 |
+// | 5,0 | 5,1 | 5,2 | 5,3 | 5,4 | 5,5 | 5,6 |
+
+function gameBoard() {
+    // console.log("finish gameBoard(). ");
+    let placeDisc =document.querySelector('.placeDisc');
+     //check to see if any player has won.
+    if(checkForWin()){
+        placeDisc.removeEventListener('click', placeDisc, false)
+    }
+
+    for (col = 0; col<=6; col++) {
+        for (row=0; row<=5; row++) {
+            //Set the inner HTML of the square (a td) to be a span with the class of 'piece' and 'player' + the value of that 
+            let draw =document.getElementById(row+'_'+col);
+            draw.innerHTML ="<span class='piece player"+gameboard[row][col]+"'> </span>";
+        }	
+    }
+    
+}
